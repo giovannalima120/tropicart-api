07f1f19b2eaa5f70849786ca93e0f70a1ffb1ec6
giovanna lima de souza <giovanna.lima1@aluno.ifsp.edu.br>
2025-06-25 14:35:30 -0300

    serviços finalizados

--- models/vaga.py
+++ models/vaga.py
@@ -1,12 +1,18 @@
 class Vaga:
 
-    idAtual = 1
-    vagas = []
-
-    def __init__ (self, titulo, descricao, local, data):
-        self.id = Vaga.idAtual
+    def __init__ (self, id, titulo, descricao, local, data):
+        self.id = id
         self.titulo = titulo
         self.descricao = descricao
         self.local = local
         self.data = data
-        
+    
+    def to_dict(self):
+        return{
+            "id" : self.id,
+            "titulo" : self.titulo,
+            "descricao": self.descricao,
+            "local" : self.local,
+            "data": self.data
+        }
+
---
+++ services/vaga_service.py
@@ -0,0 +1,47 @@
+from models.vaga import Vaga
+
+idAtual = 1
+vagas = []
+
+def gerarId():
+    global idAtual
+    idAtual += 1
+    return idAtual
+
+def listarVagas():
+    lista = [u.to_dict() for u in vagas]
+    return lista
+    
+def listarVagas(categoria=None, local=None):
+    resultado = vagas 
+
+    if categoria:
+        resultado = [v for v in resultado if v.categoria.lower() == categoria.lower()]
+    if local:
+        resultado = [v for v in resultado if v.local.lower() == local.lower()]
+
+    return resultado
+
+
+def listarVaga(id):
+    for u in vagas:
+        if u.id == id:
+            return u
+    return None
+
+
+def criarVaga(dados):
+    vaga = Vaga(gerarId(), dados["titulo"], dados["descricao"], dados["local"], dados["data"])
+    empresas.append(vaga)
+    return vaga.to_dict()
+
+
+def deletarEmpresas(id):
+    global empresas
+
+    empresaEncontrada = listarVaga(id)  
+    if empresaEncontrada:
+        empresas = [u for u in empresas if u.id != id]   
+        return True
+    else: 
+        return False  
